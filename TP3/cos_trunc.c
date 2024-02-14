028d440880c0c465130b6e8e44c2e44fa31483b0
Ara <115375868+UnLoupBlanc@users.noreply.github.com>
2024-02-14 15:53:09 +0100

    Update cos_trunc.c

--- TP3/cos_trunc.c
+++ TP3/cos_trunc.c
@@ -1,3 +1,15 @@
+//  Pour toutes les valeurs flottantes x lues sur l'entrée standard,
+//    affiche sur la sortie standard,
+//    pour la fonction f : x --> cos(x),
+//    les troncatures à l'ordre 2n
+//    de son développement en série au voisinage de 0,
+//    pour n allant de 0 à 9,
+//    puis f(x).
+//  L'affichage de chacune des troncatures est suivi d'une tabulation et de
+//    l'affichage de n. Les troncatures et la valeur f(x) sont affichées sur des
+//    lignes séparées. Chacune des valeurs flottantes est affichée sur une
+//    largeur (minimale) de 13 caractères et avec une précision de 10 décimales.
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,7 +35,8 @@ int main(void) {
   //        par le dernier appel à scanf égale 1
   //  QC : nombre d'appels à scanf
   while (scanf("%lf", &x) == 1) {
-    //  IB : 0 <= k && k <= N_MAX && s = somme((-1)^j * x^(2j) / (2j)!) && t = (-1)^k * x^(2k) / (2k)!
+    //  IB : 0 <= k && k <= N_MAX && s = somme((-1)^j * x^(2j) / (2j)!)
+    //    && t = (-1)^k * x^(2k) / (2k)!
     //    && les lignes avec les troncatures en x de 0 à k - 1 ont été affichées
     //        conformément à la spécification
     //  QC : k
